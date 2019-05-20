76e2da053bfb299807c95b1e8e16f3691e9677eb
lizengcai <lizengcai_vip@sina.com>
2019-05-21 00:48:46 +0800

    test

--- src/page/authed.vue
+++ src/page/authed.vue
@@ -11,10 +11,7 @@
     export default {
         created(){
             Cookies.set('openid', this.$route.params.openid)
-            setTimeout(function(){
-                window.location.href += '/#/'+this.$route.params.uri
-            },5000)
-
+            window.location.href += '/#/'+this.$route.params.uri
         }
     }
 </script>
