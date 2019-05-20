783733930d3e91a8e591973224809d61d2510d85
lizengcai <lizengcai_vip@sina.com>
2019-05-21 00:55:28 +0800

    test

--- src/config/router.js
+++ src/config/router.js
@@ -199,7 +199,10 @@ routes.forEach(route => {
   route.path = route.path || '/' + (route.name || '');
 });
 
-const router = new Router({ routes });
+const router = new Router({
+  mode: 'history',
+  routes
+});
 
 router.beforeEach((to, from, next) => {
   const title = to.meta && to.meta.title;
