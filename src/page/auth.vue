2c429e33c395e038178daf35458ad87da6dcbee9
lizengcai <lizengcai_vip@sina.com>
2019-05-21 00:36:19 +0800

    test

--- src/page/auth.vue
+++ src/page/auth.vue
@@ -15,7 +15,7 @@
             if (typeof(Cookies.get('openid')) === 'undefined') {
                 window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxf1e615ca4cb5c701&redirect_uri='+encodeURIComponent('http://api.lizengyi.com/index.php?s=index/Api/saveUserInfo&uri=auth')+'&response_type=code&scope=snsapi_userinfo&state=#wechat_redirect'
             }
-            console.log(openid)
+            console.log(Cookies.get('openid'))
         }
     }
 </script>
