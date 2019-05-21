7e81bb2a3d4c3d470881c4f75dc62c0161903a42
lizengcai <lizengcai_vip@sina.com>
2019-05-21 23:19:07 +0800

    test

--- src/page/product/list.vue
+++ src/page/product/list.vue
@@ -207,6 +207,15 @@ export default {
       scrollBottom() {
           if (((window.screen.height + document.body.scrollTop) > (document.body.clientHeight))){
               this.tips = '努力加载中...';
+              axios.get("http://api.lizengyi.com/index.php",{
+                  params: {
+                      s: "index/Api/getBookList",
+                      page: this.page+1,
+                      typeID: 197,
+                  }
+              }).then(response => {
+                  this.booklist.push(response.data)
+              });
           }
       }
   },
@@ -223,7 +232,6 @@ export default {
             }
         }).then(response => {
             this.booklist = response.data
-            console.log(response.data)
         });
     }
 };
