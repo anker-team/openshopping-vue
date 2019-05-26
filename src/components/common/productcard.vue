1d5422009b4198930ad3e0d1f0866d8545c62ef2
lizengcai <lizengcai_vip@sina.com>
2019-05-27 01:19:52 +0800

    fds

--- src/components/common/productcard.vue
+++ src/components/common/productcard.vue
@@ -21,9 +21,9 @@
                     </span>
                     <van-tag v-if="product.tags!=null" v-for="tag in product.tags" :key="tag" plain type="danger">{{tag}}</van-tag>
                 </p>
-                <p>
+                <div style="position: relative; bottom: -0.5em;">
                     <van-button plain type="primary" size="mini">删除</van-button><span></span>
-                </p>
+                </div>
                 <van-stepper v-if="iscard" v-model="product.num" :max="product.kucun"  :min="product.min" @change="onChange(product.id,product.num)"/>
             </template>
         </van-card>
