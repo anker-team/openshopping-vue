5d834b5d5e9270b39f3d41c80742671a641b0b97
lizengcai <lizengcai_vip@sina.com>
2019-06-03 00:01:53 +0800

    sdf

--- src/components/page/product1.vue
+++ src/components/page/product1.vue
@@ -1,16 +1,20 @@
 <template>
-<div>
+<div style="background-color: #efefef">
     <ul :class="'cap-goods-list__container cap-goods-list__container--'+data.classname+' cap-goods-list__container--'+data.ParameterDictionary.showtype+'   '+(data.ParameterDictionary.type=='6'?'nowrap':'')" >
         <li v-if="productlist.length==0" style="width:100%;height:150px;border:0px;">
             <div style="width:100%;height:150px;"></div>
         </li>
-        <li v-for="(item,index) in productlist" :key="index" :class="'cap-goods-list__wrapper  '+(data.ParameterDictionary.type=='3'?(index%3==0?'cap-goods-list__wrapper--hybrid-big ':'cap-goods-list__wrapper--hybrid-small '):'')">
+        <div style="text-align: center; width:100%">
+
+        <span style="font-weight:normal ; ;font-size:0.5rem;">为您推荐</span>
+        </div>
+        <li v-for="(item,index) in productlist" :key="index" style="width:31%;background-color: #fff;margin:0.08rem;" :class="'cap-goods-list__wrapper  '+(data.ParameterDictionary.type=='3'?(index%3==0?'cap-goods-list__wrapper--hybrid-big ':'cap-goods-list__wrapper--hybrid-small '):'')">
             <router-link :class="'cap-goods-list__item cap-goods-list__item--'+data.classname+' '+data.ParameterDictionary.showtype+' '+data.aclass" :to="'/product/'+item.id">
                 <div class="cap-goods-list__photo">
-                    <img class="cap-goods-list__img lazy lazyload" v-lazy="item.subpic+'?w='+((data.ParameterDictionary.type=='1'||data.ParameterDictionary.type=='3')?'750':'375')" />
+                    <img class="cap-goods-list__img lazy lazyload" v-lazy="item.subpic+'?w='+((data.ParameterDictionary.type=='1'||data.ParameterDictionary.type=='3')?'750':'375')" style="height:100%;width:auto" />
                 </div>
                 <div :class="'cap-goods-list__info has-title has-price '+(data.ParameterDictionary.showtype == 'card'?'has-btn':'')">
-                    <h3 class="title">{{item.title}}</h3>
+                    <h3 class="title" style="text-align: center;height: 24px;line-height: 28px">{{item.title}}</h3>
                     <p class="sale-info">
                         <span class="sale-price">¥ {{item.price_d}}</span>
                     </p>
